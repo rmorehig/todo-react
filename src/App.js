614d0d4b6353a98e2547cc62733b3bc9e031eb26
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-07 10:40:37 +0200

    Map state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h2>React Todos</h2>
-      </div>
-      <div className="Todo-App">
-        <form>
-          <input type="text"/>
-        </form>
-        <div className="Todo-List">
-          <ul>
-            <li>Learn JSX</li>
-          </ul>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos : [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an awsome app', isComplete: false},
+        {id: 3, name: 'Ship it!', isComplete: false}
+      ],
+    }
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.state.todos.map(todo => 
+                <li key={todo.id}>
+                  <input type="checkbox" checked={todo.isComplete}/>{todo.name}
+                  </li>)} 
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
