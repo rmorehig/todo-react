5b2d080baf69bc8edaf899da6a8bdcd4cd9b7468
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-08 12:42:34 +0200

    findById

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo} from './todoHelpers'
+import {addTodo, findById} from './todoHelpers'
 
 test('addTodo should add the passed todo to the list', () => {
     const startTodos = [
@@ -37,5 +37,19 @@ test('addTodo should not mutate the existing todo array', () => {
     const result = addTodo(startTodos, newTodo);
 
     expect(result).not.toBe(startTodos);
+});
+
+test('findById should return the expected item from an array', () => {
+    const startTodos = [
+        {id:1, name:'one', isComplete: false},
+        {id:2, name:'two', isComplete: false},
+        {id:3, name:'three', isComplete: false},
+    ];
+
+    const expected = {id:2, name:'two', isComplete: false};
+
+    const result = findById(2, startTodos);
+
+    expect(result).toEqual(expected);
 });
 
