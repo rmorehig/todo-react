73aa1c2913a188437779e4362d03859949a7d3ce
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-08 19:58:12 +0200

    Handling routes

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+
 export class Link extends Component {
+
+    static contextTypes= {
+        route: PropTypes.string,
+        linkHandler: PropTypes.func,
+    }
 
     handleClick = (evt) => {
         //evitamos que se recargue la página
         evt.preventDefault();
-        window.history.pushState(null,'',this.props.to)
+        //window.history.pushState(null,'',this.props.to)
+        this.context.linkHandler(this.props.to)
     }
 
     render() {
+        const activeClass = this.context.route === this.props.to ? 'active' : ''
         return (
-            <a href="#" onClick={this.handleClick}>
+            <a href="#" className={activeClass} onClick={this.handleClick}>
                 {this.props.children}
             </a>
         );
