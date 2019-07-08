9ef04f6147c1baa3a26a3dad451710abdb536374
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-07-08 19:16:39 +0200

    Delete item style

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -7,7 +7,7 @@ export const TodoItem = (props) => {
     const handleRemove = partial(props.handleRemove,props.id);
     return(
         <li>
-            <a href="#" onClick={handleRemove}>X</a>
+            <span className="delete-item"><a href="#" onClick={handleRemove}>X</a></span>
             <input type="checkbox" onChange={handleToggle} 
             checked={props.isComplete}/>{props.name}
         </li>
